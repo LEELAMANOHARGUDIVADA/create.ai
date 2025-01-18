eaf77eed9f4d92e9ba8cdce9d00f9384e633159a
LEELAMANOHARGUDIVADA <leelamanohar.gudivada@gmail.com>
2025-01-18 19:39:38 +0530

    updated imagegeneration.tsx

--- client/app/imagegeneration.tsx
+++ client/app/imagegeneration.tsx
@@ -25,7 +25,6 @@ export const unsplashApi = axios.create({
 });
 
 const ImageGeneration = () => {
-  const [firstTimeLogin, setFirstTimeLogin] = useState<Boolean>(true);
   const [prompt, setPrompt] = useState<string>("");
   const [photos, setPhotos] = useState();
   const [imageGenerating, setImageGenerating] = useState(false);
@@ -54,7 +53,6 @@ const ImageGeneration = () => {
 
   return (
     <SafeAreaView className="w-full h-full bg-white">
-      {!firstTimeLogin ? (
         <View className="w-full h-full flex items-start justify-start">
           {/* Header  */}
           <Header />
@@ -105,63 +103,6 @@ const ImageGeneration = () => {
             </View>
           </ScrollView>
         </View>
-      ) : (
-        <View className="w-full h-full flex items-center justify-between py-4">
-          <View className="w-full flex flex-row items-center justify-start px-7 mt-5 gap-1">
-            <Text className="text-2xl font-rubik-bold text-black">
-              Create.ai
-            </Text>
-            <Image
-              source={images.sparkles}
-              alt="sparkle"
-              resizeMode="contain"
-              className="mb-2 w-7 h-10"
-            />
-          </View>
-          <View className="w-full px-7 mt-5 flex items-center justify-center">
-            <View className="w-full flex flex-row gap-3">
-              <Image
-                source={images.astronaut}
-                className="w-60 h-52 rounded-[50px] shadow object-center"
-              />
-              <Image
-                source={images.tiger}
-                className="w-36 h-52 rounded-[40px] shadow"
-              />
-            </View>
-            <View className="w-full flex flex-row gap-3 mt-3">
-              <Image
-                source={images.humanrobot}
-                className="w-36 h-52 rounded-[40px] shadow"
-              />
-              <Image
-                source={images.girlrobot}
-                className="w-60 h-52 rounded-[50px] shadow"
-              />
-            </View>
-
-            <View className="w-full mt-10 flex items-center justify-center">
-              <Text className="text-3xl text-black/90 font-rubik-bold text-center">
-                Easily create and spread
-              </Text>
-              <Text className="text-3xl text-black/90 font-rubik-bold text-center">
-                your artistic vision
-              </Text>
-            </View>
-          </View>
-
-          <View className="mb-5 px-5">
-            <TouchableOpacity
-              className="bg-black w-full  p-4 rounded-2xl drop-shadow"
-              onPress={() => setFirstTimeLogin(false)}
-            >
-              <Text className="text-white text-center text-md font-semibold">
-                Get Started
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      )}
     </SafeAreaView>
   );
 };
